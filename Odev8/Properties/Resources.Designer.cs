6d9941fece6b1ed21b69f28130e25056dca63c60
Ali Can <alicnn38@gmail.com>
2023-11-26 03:58:37 +0300

    C# Odevler

--- Odev8/Properties/Resources.Designer.cs
+++ Odev8/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Odev8.Properties {
+namespace Project08.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Odev8.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Odev8.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project08.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
---
+++ Project10/ICustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project10
+{
+    interface ICustomer
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+}
